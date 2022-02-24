068a1b5ae56712a0b9bbd7c1af9f23efc6d8f58d
HalfCake <100373344+HalfCake@users.noreply.github.com>
2022-02-24 10:22:44 -0700

    Add files via upload

---
+++ var ethanRoast = [_Your jokes are cringe.js
@@ -0,0 +1,13 @@
+var ethanRoast = ["Your jokes are cringe","You can stop using apostrophe's like this now","How many Ethans does it take to change a lightbulb? One, but he has to ask Jonah to do it for him."];
+var selection = prompt("enter a number between 1 and 3. And no decimals.");
+switch(true){
+    case selection = 1:
+    console.log(ethanRoast[0])
+    break;
+    case selection = 2:
+        console.log(ethanRoast[1]);
+        break;
+    case selection = 3:
+        console.log(ethanRoast[2]);
+
+}
